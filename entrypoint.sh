888f0a5613d46f911b04f8dd97b5e819b44911cb
Mark Huang <git@hyxi.dev>
2026-05-07 20:32:05 +0800

    fix: standardize variable usage and formatting in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,24 +4,24 @@
 ssh-keygen -A
 
 # Add requested IPv4 and IPv6 addresses to the loopback interface
-if [ -n "$BIND_IPS" ]; then
-	for ip in $BIND_IPS; do
-		echo "Assigning IP $ip to loopback interface (lo)..."
-		ip addr add "$ip" dev lo || echo "Warning: Failed to add $ip. Ensure you pass --cap-add=NET_ADMIN."
-	done
-fi
+BIND_IPS="${BIND_IPS:-}"
+for ip in ${BIND_IPS}; do
+	echo "Assigning IP '${ip}' to loopback interface (lo)..."
+	ip addr add "${ip}" dev lo || echo "Warning: Failed to add '${ip}'. Ensure you pass --cap-add=NET_ADMIN."
+done
 
 # Create and setup SSH users if they don't exist
-SSH_USERS=${SSH_USERS:-proxy}
-for user in $SSH_USERS; do
-	if ! id -u "${user}" >/dev/null 2>&1; then
-		echo "Creating user '${user}'..."
-		adduser -D -s /sbin/nologin "${user}"
-		echo -n "${user}:*" | chpasswd -e
-		mkdir -m 700 -p "/home/${user}/.ssh"
-		chown "${user}:" "/home/${user}/.ssh"
-		echo "User '${user}' created successfully."
+SSH_USERS="${SSH_USERS:-}"
+for user in ${SSH_USERS}; do
+	if id -u "${user}" >/dev/null 2>&1; then
+		continue
 	fi
+	echo "Creating user '${user}'..."
+	adduser -D -s /sbin/nologin "${user}"
+	echo -n "${user}:*" | chpasswd -e
+	mkdir -m 700 -p "/home/${user}/.ssh"
+	chown "${user}:" "/home/${user}/.ssh"
+	echo "User '${user}' created successfully."
 done
 
 # Start SSH daemon
